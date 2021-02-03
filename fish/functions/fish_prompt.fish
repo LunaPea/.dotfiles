c42bff2038944ef87c9913d5726009df0e403c85
WeraPea <sokneip@tuta.io>
2021-02-03 20:17:38 +0100

    fixing fish git prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -20,7 +20,7 @@ function fish_prompt --description 'Write out the prompt'
         set -g __fish_git_prompt_char_upstream_behind "↓"
     end
     if not set -q __fish_git_prompt_char_upstream_prefix
-        set -g __fish_git_prompt_char_upstream_prefix ""
+        set -g __fish_git_prompt_char_upstream_prefix " "
     end
     if not set -q __fish_git_prompt_char_stagedstate
         set -g __fish_git_prompt_char_stagedstate "●"
@@ -35,7 +35,7 @@ function fish_prompt --description 'Write out the prompt'
         set -g __fish_git_prompt_char_invalidstate "✖"
     end
     if not set -q __fish_git_prompt_char_cleanstate
-        set -g __fish_git_prompt_char_cleanstate "✔"
+        set -g __fish_git_prompt_char_cleanstate ""
     end
     if not set -q __fish_git_prompt_color_dirtystate
         set -g __fish_git_prompt_color_dirtystate blue
